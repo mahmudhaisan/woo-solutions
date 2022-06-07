2346b7b354b8c00a5d2dad80c415356f4975b4f9
Mahmud Haisan <mahmudhaisan@gmail.com>
2022-06-07 16:28:55 +0600

    main class creation &  init using singleton pattern

--- woo-solutions.php
+++ woo-solutions.php
@@ -1,11 +1,10 @@
 <?php
 
-
 /**
  * Plugin Name: Woo All In One Solutions
- * Plugin URI: http://mahmudhaisan.com/
+ * Plugin URI: https://github.com/mahmudhaisan/woo-solutions
  * Description: This plugin is based on listing search functionality including custom post type , popular search tags etc
- * Version: 2.0
+ * Version: 1.0
  * Author: Mahmud haisan
  * Author URI: https://github.com/mahmudhaisan
  * Developer: Mahmud Haisan
@@ -15,3 +14,52 @@
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
+
+if (!defined('ABSPATH')) {
+    die;
+}
+
+/**
+ * The Main Plugin Class
+ */
+
+final class Woo_Solutions_Class {
+
+    /**
+     * Class Constructor
+     */
+
+    private function __construct() {
+
+    }
+
+    /**
+     * Initialize Singleton Instance
+     */
+
+    static function init() {
+        static $instance = false;
+
+        if (!$instance) {
+            $instance = new self();
+        }
+
+        return $instance;
+    }
+
+}
+
+/**
+ * Initialize Main Plugin
+ *
+ * @return \Woo_Solutions_Class
+ */
+
+function woo_solutions() {
+
+    return Woo_Solutions_Class::init();
+
+}
+
+// calling the main class instance
+woo_solutions();
