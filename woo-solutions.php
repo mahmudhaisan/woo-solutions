4380d073e24261850fe04c94c9f0a1b0cd2db3c9
Mahmud Haisan <mahmudhaisan@gmail.com>
2022-06-07 14:33:13 +0600

    Woocommerce All In One Solutions

---
+++ woo-solutions.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+ * Plugin Name: Woo All In One Solutions
+ * Plugin URI: http://mahmudhaisan.com/
+ * Description: This plugin is based on listing search functionality including custom post type , popular search tags etc
+ * Version: 2.0
+ * Author: Mahmud haisan
+ * Author URI: https://github.com/mahmudhaisan
+ * Developer: Mahmud Haisan
+ * Developer URI: https://github.com/mahmudhaisan
+ * Text Domain: woosolutions493
+ * Domain Path: /languages
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
